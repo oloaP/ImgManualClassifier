388d2f67f32f08c314aae01948062a8c223018a9
oloaP <paolo.devathaire@gmail.com>
2017-09-08 12:45:50 +0200

    Display images from directory

--- main_window.py
+++ main_window.py
@@ -18,11 +18,16 @@
 #   You should have received a copy of the GNU General Public License
 #   along with ImgClassifier. If not, see <http://www.gnu.org/licenses/>.
 #
+from os import walk
 
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtWidgets import QHBoxLayout, QLabel
 from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QVBoxLayout
 from PyQt5.QtWidgets import QWidget
+from os import path
 
 
 class MainWindow(QWidget):
@@ -33,6 +38,9 @@ class MainWindow(QWidget):
         self.buttons_layout = QVBoxLayout()
         self.open_folder_button = QPushButton(_("Open images folder"))
         self.image_widget = QLabel()
+        self.image_dir = None
+        self.img_queue = None
+        self.current_img_path = None
 
         self.init_ui()
 
@@ -42,7 +50,27 @@ class MainWindow(QWidget):
         self.layout.addWidget(self.image_widget)
         self.layout.addLayout(self.buttons_layout)
 
+        self.open_folder_button.clicked.connect(self.select_img_folder)
+
         self.setLayout(self.layout)
         self.showMaximized()
         self.show()
 
+    def select_img_folder(self):
+        self.image_dir = str(QFileDialog.getExistingDirectory(parent=None, caption=_('Select a folder:'),
+                                                              options=QFileDialog.ShowDirsOnly))
+        if self.image_dir:
+            self.img_queue = self._walk_dir(self.image_dir)
+            self.next_image()
+
+    def next_image(self):
+        self.current_img_path = next(self.img_queue)
+        self.image_widget.setPixmap(QPixmap(self.current_img_path).scaled(
+            self.image_widget.width(), self.image_widget.height(),
+            Qt.KeepAspectRatio))
+
+    @staticmethod
+    def _walk_dir(dir_):
+        for (dirpath, _, filenames) in walk(dir_):
+            for filename in filenames:
+                yield path.abspath(path.join(dirpath, filename))
